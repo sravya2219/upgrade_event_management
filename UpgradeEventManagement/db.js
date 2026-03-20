33b82f4e4b6a510aeb61ae59102522192e89f3e3
sravya <yaragorlasravya@gmail.com>
2026-03-20 11:44:48 +0530

    db.js

--- UpgradeEventManagement/db.js
+++ UpgradeEventManagement/db.js
@@ -1,23 +1,25 @@
 let db;
 
-let request = indexedDB.open("UpgradEventsDB",2);
+// Open DB with version 1
+let request = indexedDB.open("UpgradEventsDB", 1);
 
-request.onupgradeneeded = function(event)
-{
+request.onupgradeneeded = function(event) {
     db = event.target.result;
 
-    let store = db.createObjectStore("events", { keyPath: "id" });
-
-    store.createIndex("name", "name", { unique: false });
-    store.createIndex("category", "category", { unique: false });
+    // Create store only if it doesn't exist
+    if (!db.objectStoreNames.contains("events")) {
+        let store = db.createObjectStore("events", { keyPath: "id" });
+        store.createIndex("name", "name", { unique: false });
+        store.createIndex("category", "category", { unique: false });
+        console.log("Object store 'events' created");
+    }
 };
 
-request.onsuccess = function(event)
-{
+request.onsuccess = function(event) {
     db = event.target.result;
+    console.log("Database opened successfully");
 };
 
-request.onerror = function()
-{
+request.onerror = function() {
     console.log("Database failed to open");
 };
